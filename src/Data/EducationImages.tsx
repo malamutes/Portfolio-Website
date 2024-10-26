53f929601fafd1ecf681d413461f895db23b8aad
malamutes <hehexdroflmfao@gmail.com>
2024-10-26 13:53:19 +0800

    Test5

--- src/Data/EducationImages.tsx
+++ src/Data/EducationImages.tsx
@@ -1,8 +1,8 @@
 const EducationImages = [
-    "/applecross_shs_logo-removebg-preview (1).png",
+    "applecross_shs_logo-removebg-preview (1).png",
     "https://www.coxarchitecture.com.au/wp-content/uploads/2019/08/DSC3139_2000px-1217x810.jpg",
     "https://www.internationalinternships.com.au/assets/uploads/2021/11/curtin.png",
-    "/Curtin_Building_215_from_west.jpg"
+    "Curtin_Building_215_from_west.jpg"
 
 ]
 
