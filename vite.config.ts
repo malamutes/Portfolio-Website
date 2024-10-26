a2e676f8a9b69d9e810e7e0d1a71ae1be58536db
malamutes <hehexdroflmfao@gmail.com>
2024-10-26 22:46:42 +0800

    TitleLoad?

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Website/'
+  base: '/Portfolio-Website/', // Sets the base path for GitHub Pages
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 })
